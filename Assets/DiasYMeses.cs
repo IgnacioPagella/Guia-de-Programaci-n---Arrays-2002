12666fdc8aeca885cf78bf1ce49bdc41e2df8916
IgnacioPagella <101563475+IgnacioPagella@users.noreply.github.com>
2022-08-19 10:35:29 -0300

    clase 19/08

---
+++ Assets/DiasYMeses.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiasYMeses : MonoBehaviour
+{
+    [SerializeField] string[] nombresMeses = new string[12];
+    [SerializeField] int[] nombresDias = new int[12];
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    bool ValidarFormatoFecha(string fecha)
+    {
+        bool fechaValida = false;
+        if (fecha.Length == 4)
+        {
+            string dia = "";
+            dia += fecha[0];
+        }
+
+        return fechaValida;
+    }
+}
---
+++ Assets/Scripts/Ej1_Puntaje.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1_Puntaje : MonoBehaviour
+{
+
+    [SerializeField] int[] puntajes = new int [10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("El mayor puntaje es" + ObtenerMayorPuntaje()); 
+        Debug.Log(("El menor puntaje es" + ObtenerNivelDeMenorPuntaje()));
+        Debug.Log(("El puntaje promedio es" + ObtenerPuntajePromedio()));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    int ObtenerMayorPuntaje()
+    {
+        int mayorPuntaje = 0;
+        //int retorno = 0;
+
+        foreach (int puntaje in puntajes)
+        {
+            if(puntaje > mayorPuntaje)
+            {
+                mayorPuntaje = puntaje;
+            }
+        }
+
+        return mayorPuntaje;
+    }
+    int ObtenerNivelDeMenorPuntaje()
+    {
+        int indiceNivelMenorPuntaje = 0;
+        int menorPuntaje = puntajes[0];
+        for(int i = 0; i < puntajes.Length; i++)
+        {
+            if (puntajes[i] < menorPuntaje)
+            {
+                  menorPuntaje= puntajes[i];
+                indiceNivelMenorPuntaje = i;
+            }
+        }
+
+        return indiceNivelMenorPuntaje;
+    }
+    float ObtenerPuntajePromedio()
+    {
+        float Promedio = 0;
+
+        float suma = 0;
+        foreach (int num in puntajes)
+        {
+            suma += num;
+        }
+        Promedio = suma / puntajes.Length * 1.0f;
+
+        return Promedio;
+    }
+}
